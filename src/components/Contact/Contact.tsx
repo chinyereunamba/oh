7d537e48cd7fe2797c98abb7f5fb39d0679c8538
Chinyere <chinyereunamba17@gmail.com>
2024-05-23 02:06:49 +0100

    contact page updated

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+import "../../assets/styles/contact.css"
+
+export default function Contact() {
+    return (
+        <section className="contact">
+            <div className="contact__contact_content">
+                <h1>Let's work together.</h1>
+                <h1>
+                    <a href="mailto:cunamba2@gmail.com">Get in touch.</a>
+                </h1>
+            </div>
+        </section>
+    )
+}
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,5 @@
-// import React from 'react'
+import ContactSection from "../components/Contact/Contact"
 
 export default function Contact() {
-  return (
-    <div>Contact</div>
-  )
+    return <ContactSection />
 }
